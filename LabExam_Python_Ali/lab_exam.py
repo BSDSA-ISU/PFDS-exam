b25ae7195eabfc9b29bf308905d80d644c985eef
SugiuraAyanoo <cyrustroy.c.bazar@isu.edu.ph>
2025-09-22 17:23:32 +0800

    first test

---
+++ LabExam_Python_Ali/lab_exam.py
@@ -0,0 +1,60 @@
+print("Hello and Welcome to my lab exam output :D\n")
+
+"""
+Step 1 — Program Introduction (Introduction to Python)
+Display a welcome message using print().
+Step 2 — Variables (Variables in Python)
+Store your name (str), age (int), and GPA (float) in variables.
+Step 3 — Data Types (Types in Python)
+Use type() to display the data type of each variable.
+Step 4 — Type Conversion (Type Conversion in Python)
+Ask the user for their age as input (string).
+Convert it into an integer.
+Add 5 years and display the new age.
+Step 5 — Comments (Comments in Python)
+Add two single-line comments (#) and one multi-line comment (""" """).
+"""
+
+# Step: 2
+# This creates a variable for later use
+name = "Ali Ali"
+age = 19
+GPA = 2.2
+
+# Step: 3
+# Display what data type the variables on step 2 is
+print("the data type of name is:", type(name))
+print("the data type of age is:", type(age))
+print("the data type of GPA is:", type(GPA))
+
+print()
+
+# Step: 4
+# Asks the user whats their age using input() function
+UserAge = input("whats' your age? \n>>")
+
+# Convert UserAge to int
+UserAge = int(UserAge)
+
+# Add 5 years or 5 to it
+UserAge = UserAge + 5
+
+# Display the new age
+print("Your age on the next 5 years is:", UserAge)
+
+# Step 5: just a comment
+
+# This file/program is for my 1st sem output
+# Created in september 22 2025
+
+# Multi line comment using """
+"""
+         #####                                                          
+        #     # #   # #####  #    #  ####     ##### #####   ####  #   # 
+        #        # #  #    # #    # #           #   #    # #    #  # #  
+        #         #   #    # #    #  ####       #   #    # #    #   #   
+        #         #   #####  #    #      #      #   #####  #    #   #   
+        #     #   #   #   #  #    # #    #      #   #   #  #    #   #   
+         #####    #   #    #  ####   ####       #   #    #  ####    #   
+                                                                        
+"""
